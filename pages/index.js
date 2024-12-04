2bf76c9269d3064b7aaf0679fabd0411e202a985
Zayn Yusuf <yusue786@gmail.com>
2024-12-04 22:05:18 +0600

    fix issue

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,5 @@
-import React, { useState } from "react";
-import Header from "../components/Header";
-import Login from "../components/Login";
-import Footer from "../components/Footer";
-import Security from "../components/Security";
-import { motion } from "framer-motion";
-import useMockLogin from "../hooks/useMockLogin";
-
 export default function Home() {
-  const [showModal, setShowModal] = useState(false);
-
-  // const { login } = useMockLogin({ setShowModal });
-  return (
-    <>
-      {!showModal && (
-        <>
-          <Header />
-          <Login />
-          <Footer />
-        </>
-      )}
-
-      {showModal && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-        >
-          <Security setShowModal={setShowModal} />
-        </motion.div>
-      )}
-    </>
-  );
+  return <>Eross</>;
 }
 
 // import Footer from "../components/Footer";
